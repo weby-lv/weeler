e01425d01126a7867964cda2b082a61e02f6e4f2
Kristaps Austers <kausters@gmail.com>
2014-02-03 17:13:30 +0200

    Add icons to navbar items

--- app/helpers/weeler_view_helper.rb
+++ app/helpers/weeler_view_helper.rb
@@ -1,8 +1,16 @@
 module WeelerViewHelper
-  def navbar_link_to title, link
+  def navbar_link_to title, link, options={}
   	id = title.parameterize
-  	return %{<li class="#{id}#{' active' if @current_menu_item == id}">
-            <a href="#{link}">#{title}</a>
-          </li>}.html_safe
+  	html = %{<li class="#{id}#{' active' if @current_menu_item == id}"><a href="#{link}">}
+	if options[:icon]
+		# Available icons can be seen here: http://getbootstrap.com/components/#glyphicons
+		html << %{<span class="glyphicon glyphicon-#{options[:icon]}"#{' style="vertical-align: -1px"' if string_contains_char_from_array title, ['q','y','p','g','j']}></span>}
+	end
+  	html << %{#{title}</a></li>}
+  	return html.html_safe
+  end
+
+  def string_contains_char_from_array text, chars
+  	return !(text.chars.to_a & chars).empty?
   end
 end
