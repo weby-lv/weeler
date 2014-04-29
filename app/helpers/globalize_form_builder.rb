67ca64fc2dd12f0fa09bda816a2dd7a22830ca80
Arturs Braucs <arturs.braucs@gmail.com>
2014-04-29 16:22:28 +0300

    Globilze

--- app/helpers/globalize_form_builder.rb
+++ app/helpers/globalize_form_builder.rb
@@ -5,11 +5,11 @@ class GlobalizeFormBuilder < ActionView::Helpers::FormBuilder
     object_name = "#{@object_name}[translations_attributes][#{@index}]"
     object = @object.translations.find_or_initialize_by_locale locale.to_s
 
-    if params && params[@object_name] && params[@object_name]["translations_attributes"].present? && params[@object_name]["translations_attributes"]["#{@index}"].present?
-      params[@object_name]["translations_attributes"]["#{@index}"].each do |attribute|
-        object[attribute[0]] = attribute[1] if attribute[0].present?
-      end
-    end
+    #if params && params[@object_name] && params[@object_name]["translations_attributes"].present? && params[@object_name]["translations_attributes"]["#{@index}"].present?
+    #  params[@object_name]["translations_attributes"]["#{@index}"].each do |attribute|
+    #    object[attribute[0]] = attribute[1] if attribute[0].present?
+    #  end
+    #end
 
     @template.concat @template.hidden_field_tag("#{object_name}[id]", object ? object.id : "")
     @template.concat @template.hidden_field_tag("#{object_name}[locale]", locale)
