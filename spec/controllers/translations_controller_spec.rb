3810f0509df3552fa1acafd80af875d7f963a341
Arturs Braucs <arturs.braucs@gmail.com>
2014-11-14 12:02:26 +0200

    Fixed integration tests

--- spec/controllers/translations_controller_spec.rb
+++ spec/controllers/translations_controller_spec.rb
@@ -87,6 +87,7 @@ describe Weeler::TranslationsController do
     context "submiting a file" do
       it "adds translation from files" do
         I18n::Backend::Weeler::Translation.delete_all
+        Settings.i18n_updated_at = Time.now
 
         expect(I18n.t("welcome.title", locale: :en)).to eq("Title") # Missing translation
         post "import", file: fixture_file_upload(File.dirname(__FILE__) + '/../fixtures/test.xlsx', 'application/xlsx')
