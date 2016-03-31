682aade3cc25ac1f5405f58097156496ca70124f
Artis Raugulis <artis@devart.lv>
2016-03-31 15:44:27 +0300

    1.5.0

--- lib/weeler/version.rb
+++ lib/weeler/version.rb
@@ -1,9 +1,9 @@
 module Weeler
   module VERSION
     MAJOR = 1
-    MINOR = 4
-    TINY  = 1
-    PRE   = 4
+    MINOR = 5
+    TINY  = 0
+    PRE   = 0
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
 end
