c0cf9678df308c1b07de1d5dfd14123c846c74c3
Arturs Braucs <arturs.braucs@gmail.com>
2020-12-27 20:45:46 +0200

    Named params fixed

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,6 +2,6 @@ class Setting < RailsSettings::Base
   field :i18n_updated_at, type: :string
 
   Weeler.settings_fields.each do |field_options|
-    field field_options[:name], field_options.except(:name)
+    field field_options[:name], **field_options.except(:name)
   end
 end
